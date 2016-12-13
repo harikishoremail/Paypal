cb177f9d7f142e2ccb8bcf54a5c0485e729ef151
CH Hari Kishore <harikishoremail@gmail.com>
2016-12-13 10:20:25 +0530

    MainPage
    The Program Start Execution from this File

---
+++ PPSystem.java
@@ -0,0 +1,14 @@
+package in.msitprogram.iiit.paypal;
+
+import java.io.IOException;
+import in.msitprogram.iiit.paypal.console.MainMenu;
+
+public class PPSystem 
+{
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException 
+	{
+		MainMenu.show();
+	}
+
+}
